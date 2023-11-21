7efd6a1e2f7e9847b67d211a28725b4fabaa5697
AlexGuSpb <spbalexgus@yandex.ru>
2023-11-21 12:23:47 +0300

    task2

--- src/main/java/com/walking/intensive/chapter1/task2/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Main.java
@@ -5,13 +5,26 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int floors = 4;
+        int entrances = 2;
+        int numDoors = 33;
 
+        System.out.println(getFlatLocation(floors, entrances, numDoors));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int introductoryApatrments = 4;
+        int apartmentsEntrance = (floorAmount * introductoryApatrments) / entranceAmount;
+        int entranceNumber = (flatNumber - 1) / apartmentsEntrance + 1;
+        int floorNumber = ((flatNumber - 1) % apartmentsEntrance) / introductoryApatrments + 1;
 
-        return null; // Заглушка. При реализации - удалить
+        int position = ((flatNumber - 1) % introductoryApatrments) / 2;
+        String direction = (flatNumber % 2 == 0) ? "вправо" : "слева";
+
+        if (flatNumber > floorAmount * entranceAmount * introductoryApatrments) {
+            System.out.println("Такой квартиры нет.");
+        }
+        return  floorNumber + " кв - " + entranceNumber + " подъезд, " + position + " этаж, " + direction
+                + " от лифта.";
     }
 }
