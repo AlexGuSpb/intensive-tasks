a4a1a92c6dd8890e41c13703b277e3faf8935a2e
AlexGuSpb <spbalexgus@yandex.ru>
2023-11-26 15:01:05 +0300

    task5

--- src/main/java/com/walking/intensive/chapter1/task5/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Main.java
@@ -5,14 +5,14 @@ package com.walking.intensive.chapter1.task5;
  */
 public class Main {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
 
     }
 
     static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return 0; // Заглушка. При реализации - удалить
+
+        return 0;
     }
 
     /**
