906de9c5c61de28fee88a3b9006487ce76aed4a1
AlexGuSpb <spbalexgus@yandex.ru>
2023-11-26 14:45:30 +0300

    task2 fixed

--- src/main/java/com/walking/intensive/chapter1/task4/Main.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Main.java
@@ -13,16 +13,29 @@ public class Main {
     }
 
     static String solveQuadraticEquation(double a, double b, double c) {
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant == 0) {
-            double x = (-b) / (2 * a);
-            return "Один действительный корень: " + x;
-        } else if (discriminant > 0) {
-            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            return "Два действительных корня: " + x1 + ", " + x2;
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    return "Уравнение имеет бесконечное количество решений";
+                } else {
+                    return "Уравнение не имеет решений";
+                }
+            } else {
+                double x = -c / b;
+                return "Уравнение имеет одно решение: x = " + x;
+            }
         } else {
-            return "Нет целого решения так как дискриминант меньше нуля.";
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant > 0) {
+                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                return "Уравнение имеет два решения: x1 = " + x1 + ", x2 = " + x2;
+            } else if (discriminant == 0) {
+                double x = -b / (2 * a);
+                return "Уравнение имеет одно решение: x = " + x;
+            } else {
+                return "Уравнение не имеет решений";
+            }
         }
     }
 }
